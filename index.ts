1d1496105933a58c4e3f5a74408e8e27a3e42b66
akakispanderashvili <86373282+akakispanderashvili@users.noreply.github.com>
2023-02-06 02:00:40 +0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-import { of, concat, map } from 'rxjs';
+import { of, concat, map, interval, filter, Observable } from 'rxjs';
 
+// 1)))
+//
+//
 const a$ = of(1, 'aa', 3);
 const b$ = of(4, 5, 'bb');
 
@@ -24,4 +27,44 @@ const c$ = concat(
   )
 );
 
-c$.subscribe((val) => console.log(val));
+// c$.subscribe((val) => console.log(val));
+
+// 2)))
+//
+//
+const stream$ = interval(1000).pipe(
+  filter((value) => value % 2 === 0),
+  map((value) => value * 2)
+);
+
+// stream$.subscribe((value) => console.log(value));
+
+// 3)))
+//
+//
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+function getUsers(): Observable<String[]> {
+  const users: User[] = [
+    { firstName: 'giorgi', lastName: 'bazerashvili', age: 25 },
+    { firstName: 'meore', lastName: 'giorgi', age: 17 },
+    { firstName: 'third', lastName: 'user', age: 21 },
+  ];
+
+  return of(users).pipe(
+    map((users) => users.filter((user) => user.age >= 18)),
+    map((users) =>
+      users.map(
+        (user) => `${user.firstName} ${user.lastName}, ${user.age} years old`
+      )
+    )
+  );
+}
+
+// getUsers().subscribe((users) => {
+//   users.forEach((user) => console.log(user));
+// });
